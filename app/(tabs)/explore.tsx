4ec7e71fc43b3d6c13abdb7acd363acced1e76f4
lucasbarronio <lucas.barronio@universo.univates.br>
2024-09-27 16:37:09 -0300

    Primeiro Commit

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -1,102 +1,103 @@
-import Ionicons from '@expo/vector-icons/Ionicons';
-import { StyleSheet, Image, Platform } from 'react-native';
+import { Button, FlatList, Image, ImageBackground, StyleSheet, Text, TextInput, View } from 'react-native';
 
-import { Collapsible } from '@/components/Collapsible';
-import { ExternalLink } from '@/components/ExternalLink';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
-
-export default function TabTwoScreen() {
+export default function HomeScreen() {
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#D0D0D0', dark: '#353636' }}
-      headerImage={<Ionicons size={310} name="code-slash" style={styles.headerImage} />}>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Explore</ThemedText>
-      </ThemedView>
-      <ThemedText>This app includes example code to help you get started.</ThemedText>
-      <Collapsible title="File-based routing">
-        <ThemedText>
-          This app has two screens:{' '}
-          <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> and{' '}
-          <ThemedText type="defaultSemiBold">app/(tabs)/explore.tsx</ThemedText>
-        </ThemedText>
-        <ThemedText>
-          The layout file in <ThemedText type="defaultSemiBold">app/(tabs)/_layout.tsx</ThemedText>{' '}
-          sets up the tab navigator.
-        </ThemedText>
-        <ExternalLink href="https://docs.expo.dev/router/introduction">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Android, iOS, and web support">
-        <ThemedText>
-          You can open this project on Android, iOS, and the web. To open the web version, press{' '}
-          <ThemedText type="defaultSemiBold">w</ThemedText> in the terminal running this project.
-        </ThemedText>
-      </Collapsible>
-      <Collapsible title="Images">
-        <ThemedText>
-          For static images, you can use the <ThemedText type="defaultSemiBold">@2x</ThemedText> and{' '}
-          <ThemedText type="defaultSemiBold">@3x</ThemedText> suffixes to provide files for
-          different screen densities
-        </ThemedText>
-        <Image source={require('@/assets/images/react-logo.png')} style={{ alignSelf: 'center' }} />
-        <ExternalLink href="https://reactnative.dev/docs/images">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Custom fonts">
-        <ThemedText>
-          Open <ThemedText type="defaultSemiBold">app/_layout.tsx</ThemedText> to see how to load{' '}
-          <ThemedText style={{ fontFamily: 'SpaceMono' }}>
-            custom fonts such as this one.
-          </ThemedText>
-        </ThemedText>
-        <ExternalLink href="https://docs.expo.dev/versions/latest/sdk/font">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Light and dark mode components">
-        <ThemedText>
-          This template has light and dark mode support. The{' '}
-          <ThemedText type="defaultSemiBold">useColorScheme()</ThemedText> hook lets you inspect
-          what the user's current color scheme is, and so you can adjust UI colors accordingly.
-        </ThemedText>
-        <ExternalLink href="https://docs.expo.dev/develop/user-interface/color-themes/">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Animations">
-        <ThemedText>
-          This template includes an example of an animated component. The{' '}
-          <ThemedText type="defaultSemiBold">components/HelloWave.tsx</ThemedText> component uses
-          the powerful <ThemedText type="defaultSemiBold">react-native-reanimated</ThemedText> library
-          to create a waving hand animation.
-        </ThemedText>
-        {Platform.select({
-          ios: (
-            <ThemedText>
-              The <ThemedText type="defaultSemiBold">components/ParallaxScrollView.tsx</ThemedText>{' '}
-              component provides a parallax effect for the header image.
-            </ThemedText>
-          ),
-        })}
-      </Collapsible>
-    </ParallaxScrollView>
+    <View style={styles.container}>
+      <ImageBackground
+        style={styles.capa}
+        source={require('@/assets/images/cadastroBanner.jpg')}>
+        <View style={{ flex: 1, justifyContent: 'center', marginLeft: 20 }}><Text style={styles.cabecalho}>Cadastro</Text></View>
+      </ImageBackground>
+
+      <View style={{alignItems: 'center', paddingTop: 30}}>
+        <View style={{ paddingBottom: 10}}>
+          <Text style={styles.inputTitulos}>Título</Text>
+          <TextInput
+            inputMode='text'
+            placeholder='Digite um item para adicionar a lista'
+            placeholderTextColor={'#7c7c7c'}
+            style={styles.inputEstilo}
+          />
+        </View>
+        <View style={{ paddingBottom: 10}}>
+        <Text style={styles.inputTitulos}>Genero</Text>
+          <TextInput
+            inputMode='text'
+            placeholder='Digite um item para adicionar a lista'
+            placeholderTextColor={'#7c7c7c'}
+            style={styles.inputEstilo}
+          />
+        </View>
+        <View style={{ paddingBottom: 10}}>
+        <Text style={styles.inputTitulos}>Duracao</Text>
+          <TextInput
+            inputMode='text'
+            placeholder='Digite um item para adicionar a lista'
+            placeholderTextColor={'#7c7c7c'}
+            style={styles.inputEstilo}
+          />
+        </View>
+        <View style={{ paddingBottom: 30}}>
+        <Text style={styles.inputTitulos}>Classificacao</Text>
+          <TextInput
+            inputMode='text'
+            placeholder='Digite um item para adicionar a lista'
+            placeholderTextColor={'#7c7c7c'}
+            style={styles.inputEstilo}
+          />
+        </View>
+      </View>
+      <View style={{alignItems: 'center'}}>
+        <Button
+          title='Cadastrar'
+          color={'#35bf2e'}
+        />
+      </View>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  headerImage: {
-    color: '#808080',
-    bottom: -90,
-    left: -35,
-    position: 'absolute',
+  container: {
+    flex: 1,
+    backgroundColor: '#3c3c3b',
+    justifyContent: 'flex-start',
+    marginTop: 40
   },
-  titleContainer: {
-    flexDirection: 'row',
-    gap: 8,
+  capa: {
+    width: '100%',
+    height: 200,
   },
+  cabecalho: {
+    color: '#fff',
+    fontSize: 60,
+    fontWeight: 'bold',
+    textShadowColor: '#000',
+    textShadowOffset: { width: 3, height: 3 },
+    textShadowRadius: 1,
+  },
+  containerCabecalho: {
+    alignItems: 'center'
+  },
+  containerLista: {
+    padding: 20
+  },
+  textoLista: {
+    color: '#FFF',
+    fontSize: 15
+  },
+  inputTitulos: {
+    fontSize: 15,
+    color: '#FFF',
+    paddingBottom: 1,
+    paddingLeft: 3
+  },
+  inputEstilo: {
+    height: 40,
+    padding: 10,
+    color: '#000',
+    backgroundColor: '#d9d9d9',
+    width: 380
+  },
+  
 });
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,70 +1,102 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import { Button, FlatList, Image, ImageBackground, StyleSheet, Text, TextInput, View } from 'react-native';
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+const DATA = [
+  {
+    titulo: 'Vertigo', genero: 'Romance', duracao: '129mins', classificacao: 'A14'
+  },
+  {
+    titulo: 'Ran', genero: 'História', duracao: '168mins', classificacao: 'A14'
+  },
+  {
+    titulo: 'Aliens', genero: 'Ficção Científica', duracao: '154mins', classificacao: 'A14'
+  },
+  {
+    titulo: 'Oppenheimer', genero: 'História', duracao: '167mins', classificacao: 'A14'
+  },
+  {
+    titulo: 'O Sexto Sentido', genero: 'Terror', duracao: '173mins', classificacao: 'A14'
+  },
+  {
+    titulo: 'Fragmentado', genero: 'Thriller', duracao: '143mins', classificacao: 'A14'
+  },
+]
 
 export default function HomeScreen() {
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
+    <View style={styles.container}>
+      <ImageBackground
+        style={styles.capa}
+        source={require('@/assets/images/listaBanner.jpg')}>
+        <View style={{ flex: 1, justifyContent: 'center', marginLeft: 20 }}><Text style={styles.cabecalho}>Filmes</Text></View>
+      </ImageBackground>
+
+      <View style={{padding: 20, paddingBottom: 0}}>
+        <Button
+          title='Cadastrar'
+          color={'#2177e8'}
         />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+      </View>
+      <View style={{ flexDirection: 'row', justifyContent: 'space-between', padding: 20, paddingBottom: 0}}>
+        <TextInput
+          inputMode='text'
+          placeholder='Digite um item para adicionar a lista'
+          placeholderTextColor={'#7c7c7c'}
+          style={styles.inputEstilo}
+        />
+        <Button
+          title='Buscar'
+          color={'#35bf2e'}
+        />
+      </View>
+
+      <FlatList
+        style={styles.containerLista}
+        data={DATA}
+        renderItem={({ item }) => (
+          <View>
+            <Text style={styles.textoLista}>• {item.titulo} - {item.genero} - {item.duracao} - {item.classificacao}</Text>
+          </View>
+        )}
+        keyExtractor={item => item.titulo}
+      />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
+  container: {
+    flex: 1,
+    backgroundColor: '#3c3c3b',
+    justifyContent: 'flex-start',
+    marginTop: 40
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+  capa: {
+    width: '100%',
+    height: 200,
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+  cabecalho: {
+    color: '#fff',
+    fontSize: 60,
+    fontWeight: 'bold',
+    textShadowColor: '#000',
+    textShadowOffset: { width: 3, height: 3 },
+    textShadowRadius: 1,
   },
+  containerCabecalho: {
+    alignItems: 'center'
+  },
+  containerLista: {
+    padding: 20
+  },
+  textoLista: {
+    color: '#FFF',
+    fontSize: 20,
+    paddingBottom: 10
+  },
+  inputEstilo: {
+    height: 40,
+    color: '#000',
+    backgroundColor: '#d9d9d9',
+    width: '80%'
+  }
 });
