37f0be21d61276b7191100c41337f58f14a9a52d
lucasbarronio <lucas.barronio@universo.univates.br>
2024-10-04 17:38:36 -0300

    Versiona tela de cadastros

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -1,4 +1,4 @@
-import { Button, FlatList, Image, ImageBackground, StyleSheet, Text, TextInput, View } from 'react-native';
+import { Button, ImageBackground, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function HomeScreen() {
   return (
@@ -6,63 +6,56 @@ export default function HomeScreen() {
       <ImageBackground
         style={styles.capa}
         source={require('@/assets/images/cadastroBanner.jpg')}>
-        <View style={{ flex: 1, justifyContent: 'center', marginLeft: 20 }}><Text style={styles.cabecalho}>Cadastro</Text></View>
-      </ImageBackground>
+        <View style={styles.containerCabecalho}>
+          <Text style={styles.cabecalho}>Cadastro</Text>
+        </View>
+      </ImageBackground >
 
-      <View style={{alignItems: 'center', paddingTop: 30}}>
-        <View style={{ paddingBottom: 10}}>
-          <Text style={styles.inputTitulos}>Título</Text>
+      <View style={styles.containerMain}>
+        <View style={styles.inputBox}>
+          <Text style={styles.inputLabel}>Título</Text>
           <TextInput
             inputMode='text'
-            placeholder='Digite um item para adicionar a lista'
-            placeholderTextColor={'#7c7c7c'}
             style={styles.inputEstilo}
           />
         </View>
-        <View style={{ paddingBottom: 10}}>
-        <Text style={styles.inputTitulos}>Genero</Text>
+        <View style={styles.inputBox}>
+          <Text style={styles.inputLabel}>Gênero</Text>
           <TextInput
             inputMode='text'
-            placeholder='Digite um item para adicionar a lista'
-            placeholderTextColor={'#7c7c7c'}
             style={styles.inputEstilo}
           />
         </View>
-        <View style={{ paddingBottom: 10}}>
-        <Text style={styles.inputTitulos}>Duracao</Text>
+        <View style={styles.inputBox}>
+          <Text style={styles.inputLabel}>Duração</Text>
           <TextInput
             inputMode='text'
-            placeholder='Digite um item para adicionar a lista'
-            placeholderTextColor={'#7c7c7c'}
             style={styles.inputEstilo}
           />
         </View>
-        <View style={{ paddingBottom: 30}}>
-        <Text style={styles.inputTitulos}>Classificacao</Text>
+        <View style={styles.inputBox}>
+          <Text style={styles.inputLabel}>Classificação</Text>
           <TextInput
             inputMode='text'
-            placeholder='Digite um item para adicionar a lista'
-            placeholderTextColor={'#7c7c7c'}
             style={styles.inputEstilo}
           />
         </View>
       </View>
-      <View style={{alignItems: 'center'}}>
+      <View style={styles.cadastrarBtn}>
         <Button
           title='Cadastrar'
-          color={'#35bf2e'}
+          color={'#005671'}
         />
       </View>
-    </View>
+    </View >
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#3c3c3b',
+    backgroundColor: '#232323',
     justifyContent: 'flex-start',
-    marginTop: 40
   },
   capa: {
     width: '100%',
@@ -76,28 +69,41 @@ const styles = StyleSheet.create({
     textShadowOffset: { width: 3, height: 3 },
     textShadowRadius: 1,
   },
-  containerCabecalho: {
-    alignItems: 'center'
-  },
-  containerLista: {
-    padding: 20
+  containerMain: {
+    alignItems: 'center',
+    paddingTop: 40,
+    paddingHorizontal: 20
   },
   textoLista: {
     color: '#FFF',
-    fontSize: 15
-  },
-  inputTitulos: {
     fontSize: 15,
+  },
+  inputLabel: {
+    fontSize: 20,
     color: '#FFF',
     paddingBottom: 1,
-    paddingLeft: 3
+    paddingLeft: 3,
+    fontWeight: 'bold'
   },
   inputEstilo: {
     height: 40,
-    padding: 10,
+    padding: 5,
     color: '#000',
     backgroundColor: '#d9d9d9',
-    width: 380
+    fontSize: 20
   },
-  
+  inputBox: {
+    paddingBottom: 30,
+    width: '100%'
+  },
+  containerCabecalho: {
+    flex: 1,
+    justifyContent: 'center',
+    marginLeft: 20,
+  },
+  cadastrarBtn: {
+    paddingHorizontal:10,
+    paddingTop: 180
+  }
+
 });
