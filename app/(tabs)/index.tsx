59c55791d494540e5ad7c274198040c9b5f49936
lucasbarronio <lucas.barronio@universo.univates.br>
2024-10-04 17:39:05 -0300

    Versiona tela de listagem

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,22 +2,22 @@ import { Button, FlatList, Image, ImageBackground, StyleSheet, Text, TextInput, 
 
 const DATA = [
   {
-    titulo: 'Vertigo', genero: 'Romance', duracao: '129mins', classificacao: 'A14'
+    capa: 'https://a.ltrbxd.com/resized/sm/upload/q9/3o/ng/om/obhM86qyv8RsE69XSMTtT9FdE0b-0-1000-0-1500-crop.jpg?v=1403a5e003', titulo: 'Vertigo', genero: 'Romance', duracao: '129 mins', classificacao: 'A14'
   },
   {
-    titulo: 'Ran', genero: 'História', duracao: '168mins', classificacao: 'A14'
+    capa: 'https://a.ltrbxd.com/resized/sm/upload/h6/yo/p7/1t/ran-0-1000-0-1500-crop.jpg?v=3cabb176dc', titulo: 'Ran', genero: 'História', duracao: '168 mins', classificacao: 'A14'
   },
   {
-    titulo: 'Aliens', genero: 'Ficção Científica', duracao: '154mins', classificacao: 'A14'
+    capa: 'https://a.ltrbxd.com/resized/film-poster/5/1/4/4/5/51445-aliens-0-1000-0-1500-crop.jpg?v=6c62918bdd', titulo: 'Aliens', genero: 'Ficção Científica', duracao: '154 mins', classificacao: 'A14'
   },
   {
-    titulo: 'Oppenheimer', genero: 'História', duracao: '167mins', classificacao: 'A14'
+    capa: 'https://a.ltrbxd.com/resized/film-poster/7/8/4/3/2/8/784328-oppenheimer-0-1000-0-1500-crop.jpg?v=e3c6e7a32c', titulo: 'Oppenheimer', genero: 'História', duracao: '167 mins', classificacao: 'A14'
   },
   {
-    titulo: 'O Sexto Sentido', genero: 'Terror', duracao: '173mins', classificacao: 'A14'
+    capa: 'https://a.ltrbxd.com/resized/film-poster/5/1/4/0/7/51407-the-sixth-sense-0-1000-0-1500-crop.jpg?v=ab482dfeb6', titulo: 'O Sexto Sentido', genero: 'Terror', duracao: '173 mins', classificacao: 'A14'
   },
   {
-    titulo: 'Fragmentado', genero: 'Thriller', duracao: '143mins', classificacao: 'A14'
+    capa: 'https://a.ltrbxd.com/resized/sm/upload/5e/pw/vs/km/8CHJcVc5IGXS1sC5DyjeMwTDEQH-0-1000-0-1500-crop.jpg?v=ab64d75800', titulo: 'Fragmentado', genero: 'Thriller', duracao: '143 mins', classificacao: 'A14'
   },
 ]
 
@@ -27,25 +27,27 @@ export default function HomeScreen() {
       <ImageBackground
         style={styles.capa}
         source={require('@/assets/images/listaBanner.jpg')}>
-        <View style={{ flex: 1, justifyContent: 'center', marginLeft: 20 }}><Text style={styles.cabecalho}>Filmes</Text></View>
+        <View style={styles.containerCabecalho}>
+          <Text style={styles.cabecalho}>Filmes</Text>
+        </View>
       </ImageBackground>
 
-      <View style={{padding: 20, paddingBottom: 0}}>
+      <View style={styles.cadastroRedirect}>
         <Button
-          title='Cadastrar'
-          color={'#2177e8'}
+          title='Cadastros'
+          color={'#005671'}
         />
       </View>
-      <View style={{ flexDirection: 'row', justifyContent: 'space-between', padding: 20, paddingBottom: 0}}>
+      <View style={styles.containerPesquisa}>
         <TextInput
           inputMode='text'
-          placeholder='Digite um item para adicionar a lista'
+          placeholder='Busque por um filme'
           placeholderTextColor={'#7c7c7c'}
           style={styles.inputEstilo}
         />
         <Button
           title='Buscar'
-          color={'#35bf2e'}
+          color={'#b36736'}
         />
       </View>
 
@@ -53,12 +55,23 @@ export default function HomeScreen() {
         style={styles.containerLista}
         data={DATA}
         renderItem={({ item }) => (
-          <View>
-            <Text style={styles.textoLista}>• {item.titulo} - {item.genero} - {item.duracao} - {item.classificacao}</Text>
+          <View style={styles.itemLista}>
+            <Image
+              style={styles.previewFilme}
+              source={{ uri: item.capa }}
+            />
+            <View style={styles.dadosFilme}>
+              <Text style={styles.cabecalhoItem}>{item.titulo}</Text>
+              <View style={styles.footerFilme}>
+                <Text style={styles.textoItem}>{item.genero}</Text>
+                <Text style={styles.subtextoItem}>{item.duracao} • {item.classificacao}</Text>
+              </View>
+            </View>
           </View>
         )}
         keyExtractor={item => item.titulo}
       />
+
     </View>
   );
 }
@@ -66,9 +79,8 @@ export default function HomeScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#3c3c3b',
-    justifyContent: 'flex-start',
-    marginTop: 40
+    backgroundColor: '#232323',
+    justifyContent: 'flex-start'
   },
   capa: {
     width: '100%',
@@ -83,20 +95,68 @@ const styles = StyleSheet.create({
     textShadowRadius: 1,
   },
   containerCabecalho: {
-    alignItems: 'center'
+    flex: 1,
+    justifyContent: 'center',
+    marginLeft: 20
   },
   containerLista: {
-    padding: 20
+    paddingHorizontal: 10,
+    marginVertical: 20
   },
-  textoLista: {
+  cabecalhoItem: {
     color: '#FFF',
-    fontSize: 20,
-    paddingBottom: 10
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  subtextoItem: {
+    color: '#a9bcc2',
+    fontSize: 16,
+    fontWeight: '400',
   },
   inputEstilo: {
     height: 40,
     color: '#000',
     backgroundColor: '#d9d9d9',
-    width: '80%'
+    width: '80%',
+    fontSize: 20,
+    paddingLeft: 10
+  },
+  cadastroRedirect: {
+    paddingHorizontal: 10,
+    paddingTop: 15,
+    paddingBottom: 0,
+  },
+  containerPesquisa: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: 10,
+    paddingBottom: 0
+  },
+  itemLista: {
+    backgroundColor: '#121212',
+    flexDirection: 'row',
+    marginBottom: 14,
+    height: 90,
+  },
+  previewFilme: {
+    width: 60,
+    height: 90,
+  },
+  dadosFilme: {
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    width: '85%',
+    flexDirection: 'column',
+    justifyContent: 'space-between'
+  },
+  textoItem: {
+    color: '#a9bcc2',
+    fontSize: 18,
+    fontWeight: '500'
+  },
+  footerFilme: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
   }
 });
